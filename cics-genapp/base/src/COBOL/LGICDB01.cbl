3e2fdf8c7135220b85bba2036419721818d36e1b
Jean-Yves BAUDY <baudy.jy@fr.ibm.com>
2021-02-18 21:43:54 +0100

    IBM Cloud Integration

--- cics-genapp/base/src/COBOL/LGICDB01.cbl
+++ cics-genapp/base/src/COBOL/LGICDB01.cbl
@@ -18,7 +18,7 @@
       * Select customer details from DB2 table                         *
       *                                                                *
       *                                                                *
-      * MODIF: 45                                                      *
+      * MODIF: 46                                                      *
       ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. LGICDB01.
